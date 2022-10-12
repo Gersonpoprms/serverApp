615ccfe8bf28b172b6c0590b3ba97c23018eab80
gersonpoprms <114246745+Gersonpoprms@users.noreply.github.com>
2022-10-12 11:39:38 -0500

    disable token

--- src/routes/formats.routes.js
+++ src/routes/formats.routes.js
@@ -3,13 +3,17 @@ import * as formatsCtrl from "../controllers/formats.controller.js";
 import { verifyToken } from "../middelwares/index.js";
 const router = Router();
 
-router.get('/:type', verifyToken, formatsCtrl.getFormats);
-router.post('/',verifyToken, formatsCtrl.createFormat);
-router.get('/:type/:id', verifyToken, formatsCtrl.getFormatById);
-router.put('/', verifyToken, formatsCtrl.updateFormat);
-router.put('/:type/:id',verifyToken, formatsCtrl.undeleteFormat);
-router.delete('/:type/:id',verifyToken, formatsCtrl.deleteFormatById);
-router.purge('/:type/:id/',verifyToken, formatsCtrl.eraseFormatById);
+/*        type => Nombre de la tabla GTHFOR086
+          id   => clave primaria, nombre estandar  IdFormat_PK          */
+
+
+router.get('/:type', /* verifyToken, */ formatsCtrl.getFormats);
+router.post('/',/* verifyToken, */ formatsCtrl.createFormat);
+router.get('/:type/:id', /* verifyToken, */ formatsCtrl.getFormatById);
+router.put('/', /* verifyToken, */ formatsCtrl.updateFormat);
+router.put('/:type/:id',/* verifyToken, */ formatsCtrl.undeleteFormat);
+router.delete('/:type/:id',/* verifyToken, */ formatsCtrl.deleteFormatById);
+router.purge('/:type/:id/',/* verifyToken, */ formatsCtrl.eraseFormatById);
 
 
 export default router;
